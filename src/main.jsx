3f22cfb55d5e055e765b25862cb29292e3e52283
Adika Ifeanyi <adika.emeka@gmail.com>
2025-12-26 17:44:59 +0100

    Add Hall of Fame route to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-
-import { AuthProvider } from './context/AuthContext'  // ← ADD THIS
+import { AuthProvider } from './context/AuthContext'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import ToDoList from './pages/ToDoList'
 import Leaderboard from './pages/Leaderboard'
 import Friends from './pages/Friends'
+import HallOfFame from './pages/HallOfFame'
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem('token')
@@ -18,7 +18,7 @@ const ProtectedRoute = ({ children }) => {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>  {/* ← ADD THIS */}
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
@@ -27,9 +27,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/todos" element={<ProtectedRoute><ToDoList /></ProtectedRoute>} />
           <Route path="/leaderboard" element={<ProtectedRoute><Leaderboard /></ProtectedRoute>} />
           <Route path="/friends" element={<ProtectedRoute><Friends /></ProtectedRoute>} />
+          <Route path="/hall-of-fame/:groupId" element={<ProtectedRoute><HallOfFame /></ProtectedRoute>} />
           <Route path="/" element={<Navigate to="/dashboard" />} />
         </Routes>
       </BrowserRouter>
-    </AuthProvider>  {/* ← ADD THIS */}
+    </AuthProvider>
   </React.StrictMode>,
 )
