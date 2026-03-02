6a52c34ba1dfee7878ea0210d4e14684e32d26f4
Ifeaanyi <adika.emeka@gmail.com>
2026-03-02 06:08:07 +0000

    fix: exclude habits deleted on first day of month from that month

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -351,7 +351,7 @@ const habitsForMonth = habits.filter(h => {
   const createdDate = h.created_at ? h.created_at.split('T')[0] : '2000-01-01';
   const deletedDate = h.deleted_at ? h.deleted_at.replace('T', ' ').split(' ')[0] : null;
   const wasCreatedBeforeMonthEnd = createdDate <= monthEnd;
-  const wasNotDeletedBeforeMonthStart = !deletedDate || deletedDate >= monthStart;
+  const wasNotDeletedBeforeMonthStart = !deletedDate || deletedDate > monthStart;
   return wasCreatedBeforeMonthEnd && wasNotDeletedBeforeMonthStart;
 });
 
